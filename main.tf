2de412502135f957c559ce624de7672acbebd146
David Allen <titansdfa86@gmail.com>
2019-01-01 10:42:29 -0500

    Fixed user_data

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "example" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
-  user_date = <<-EOF
+  user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
