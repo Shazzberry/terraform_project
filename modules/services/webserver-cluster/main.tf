be64a57096581dcc2822ab573738c4b36f4fa081
David Allen <30960599+Shazzberry@users.noreply.github.com>
2019-01-15 18:38:54 -0500

    Update main.tf

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -77,21 +77,27 @@ resource "aws_elb" "example" {
 }
 
 resource "aws_security_group" "elb" {
-  name = "${var.cluster_name}-elb"
+  name = "${var.cluster_name}-elb"  
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "allow_http_inbound"{
+  type               = "ingress"
+  security_group_id  = "${aws_security_group_elb.id}"
+  
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = "${aws_security_group.elb.id}"
+  
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 data "terraform_remote_state" "db" {
