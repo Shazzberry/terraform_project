11c58e746070d858036605e6987591924a7ff62f
David Allen <titansdfa86@gmail.com>
2019-01-15 11:09:35 -0500

    Move dir locations

---
+++ modules/services/webserver-cluster/var.tf
@@ -0,0 +1,16 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket for the database's remote state"
+}
+
+variable "db_remote_state_key" {
+  description = "The path for the database's remote state in S3"
+}
